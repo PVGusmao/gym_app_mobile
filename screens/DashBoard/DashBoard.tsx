93b135abb5631d2e0abbb3ac24aba668a11303c5
PVMGusmao <paulo.gusmao@stellabarros.com.br>
2023-02-15 18:20:32 -0300

    alguns testes com a api de exercicios

--- screens/DashBoard/DashBoard.tsx
+++ screens/DashBoard/DashBoard.tsx
@@ -1,7 +1,8 @@
+import axios from "axios";
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
-import BackgroundVideo from "../../components/BackgroundVideo/BackgroundVideo";
-
+import { ButtonComponent } from "../../components/Button/Button";
+import api from "../../utils/api";
 
 export function DashBoard() {
   return (
@@ -12,7 +13,18 @@ export function DashBoard() {
     }}>
       <StatusBar translucent/>
       <Text>Fitness Center</Text>
-      {/* <BackgroundVideo /> */}
+
+      <ButtonComponent
+        title="Montar a sua série"
+        mt={20}
+        bRadius={20}
+        onPress={async () => {
+          // const response = await axios.get('https://api.api-ninjas.com/v1/exercises?muscle=biceps', { headers: {'X-Api-key': 'ulZm8Uqc0oa8GsEVSXS6Eg==OKWFsibXkKvrqO8S'}});
+          // console.log(response.data);
+          const response = await api.get('/exercise');
+          console.log(response.data);
+        }}
+      />
     </View>
   )
 }
